b18e276cb8d04c6692f2ad890542eb5ebbee21bf
Ivan Berlin <ivan.berlin06@gmail.com>
2023-03-11 12:36:41 +0300

    upd login link open func

--- src/components/popups/Login.tsx
+++ src/components/popups/Login.tsx
@@ -3,10 +3,12 @@ import {
 } from '@mantine/core';
 import { invoke } from '@tauri-apps/api/tauri';
 import { Link } from 'react-router-dom';
+import { open } from '@tauri-apps/api/shell';
 
 const MICROSOFT_LINK = 'https://login.microsoftonline.com/consumers/oauth2/v2.0/authorize?client_id=d8e1d9bf-287f-4773-a176-e012722257f4&response_type=code&redirect_uri=http://localhost:9397&scope=XboxLive.signin%20offline_access&state=NOT_NEEDED'
 
 async function addAccount() {
+    open(MICROSOFT_LINK)
     try {
         await invoke('add_account');
         console.log(Response);
@@ -23,7 +25,7 @@ export function Login() {
                 <Title order={1}>
                     Login with your Microsoft account
                 </Title>
-                <Button onClick={addAccount} variant='outline' target="_blank" component="a" href={MICROSOFT_LINK} >
+                <Button onClick={addAccount} variant='outline'>
                     Login
                 </Button>
             </Flex>
