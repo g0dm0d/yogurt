e9c987fe635816c741aecb46a553f3c1fa55f4ba
ModerNik <DeOlejek@gmail.com>
2023-03-11 12:38:04 +0300

    update

--- src/components/popups/Login.tsx
+++ src/components/popups/Login.tsx
@@ -2,7 +2,6 @@ import {
     Box, Button, Flex, Title
 } from '@mantine/core';
 import { invoke } from '@tauri-apps/api/tauri';
-import { Link } from 'react-router-dom';
 import { open } from '@tauri-apps/api/shell';
 
 const MICROSOFT_LINK = 'https://login.microsoftonline.com/consumers/oauth2/v2.0/authorize?client_id=d8e1d9bf-287f-4773-a176-e012722257f4&response_type=code&redirect_uri=http://localhost:9397&scope=XboxLive.signin%20offline_access&state=NOT_NEEDED'
