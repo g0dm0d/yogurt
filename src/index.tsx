6ac87816447acaefc3bf844cc8048f30685815be
Ivan Berlin <ivan.berlin06@gmail.com>
2023-03-11 23:42:40 +0300

    color pallet upd

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <MantineProvider withGlobalStyles withNormalizeCSS theme={{
         colorScheme: 'dark',
         colors: {
-            yogurt: ['#F0BBDD', '#ED9BCF', '#EC7CC3', '#ED5DB8', '#F13EAF', '#F71FA7', '#FF00A1', '#E00890', '#C50E82', '#AD1374'],
+            yogurt: ['#ffe2e2', '#ffd8d8', '#ffcfcf', '#ffc6c6', '#ffbdbd', '#ffb4b4', '#ffabab', '#ffa2a2', '#ff9999','#ff9090'],
         },
         primaryColor: 'yogurt',
         globalStyles: (theme) => ({
