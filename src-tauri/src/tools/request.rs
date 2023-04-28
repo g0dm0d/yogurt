34ce6471d2852b1551e982a8793402a078763616
Ivan Berlin <ivan.berlin06@gmail.com>
2023-04-28 20:31:27 +0300

    update get func

--- src-tauri/src/tools/request.rs
+++ src-tauri/src/tools/request.rs
@@ -1,20 +1,16 @@
-use reqwest::{Error, StatusCode};
-use std::{io, time::Duration};
+use reqwest::Error;
+use std::io;
 
 pub async fn get(url: &str) -> Result<io::Cursor<bytes::Bytes>, Error> {
-    let mut response = reqwest::get(url).await.expect("request failed");
     loop {
-        match response.status() {
-            StatusCode::OK => {
-                let content = io::Cursor::new(response.bytes().await.expect("body invalid"));
+        match reqwest::get(url).await {
+            Ok(resp) => {
+                let content = io::Cursor::new(resp.bytes().await.expect("body invalid"));
                 return Ok(content);
             }
-            status if status.is_server_error() => {
-                response = reqwest::get(url).await.expect("request failed");
-            }
-            _ => {
-                tokio::time::sleep(Duration::from_secs(5)).await;
-                response = reqwest::get(url).await.expect("request failed");
+            Err(err) => {
+                eprintln!("Request error: {}", err);
+                continue;
             }
         }
     }
